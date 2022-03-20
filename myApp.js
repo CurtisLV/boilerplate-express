30f74baa07c5a9006fcf88a20f52c5a4f70c5a35
CurtisLV <curtislv@gmail.com>
2022-03-20 21:05:52 +0200

    express fcc

--- myApp.js
+++ myApp.js
@@ -1,5 +1,28 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
+
+console.log('Hello World');
+
+// app.get('/', (req, res) => {
+//     res.send('Hello Express');
+// });
+
+absolutePath = __dirname + '/views/index.html';
+
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
+});
+
+abolPath2 = __dirname + '/public';
+app.use('/public', express.static(abolPath2));
+
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({ message: 'HELLO JSON' });
+    } else {
+        res.json({ message: 'Hello json' });
+    }
+});
 
 module.exports = app;
-console.log('Hello World');
